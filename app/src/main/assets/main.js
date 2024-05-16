f9ce307a2b618b522aa2574777ea8d15b8c456d2
Albert Lahat <lahat.albert@gmail.com>
2024-05-16 16:30:17 +0100

    improve layout no more overlaps in the clock

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -44,9 +44,7 @@ function toggleTrail() {
     showTrail = document.getElementById("trailCheckbox").checked;
 }
 function toggleClock() {
-    document.getElementById("clock").style.opacity = document.getElementById("clockCheckbox").checked ? ".25" : "0";
-    document.getElementById("date").style.opacity = document.getElementById("clockCheckbox").checked ? ".25" : "0";
-    document.getElementById("onThisDate").style.opacity = document.getElementById("clockCheckbox").checked ? ".25" : "0";
+    document.getElementById("centralClock").style.opacity = document.getElementById("clockCheckbox").checked ? ".4" : "0";
 }
 function toggleInfo() {
     document.getElementById("info").style.opacity = document.getElementById("infoCheckbox").checked ? "1" : "0";
