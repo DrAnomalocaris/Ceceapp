c6d62432f366db6d42d0bc3be479bd45beae99a3
Albert Lahat <lahat.albert@gmail.com>
2024-05-17 10:50:38 +0100

    trying to keep it fullscreen

--- app/src/main/java/com/albert/ceceapp/MainActivity.java
+++ app/src/main/java/com/albert/ceceapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.albert.ceceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -13,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the SYSTEM_UI_FLAG_FULLSCREEN flag to make the app appear fullscreen
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+
+
+
         // Set the FLAG_KEEP_SCREEN_ON flag to keep the screen on
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
